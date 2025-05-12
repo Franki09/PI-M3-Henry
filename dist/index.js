76281b71168b07b70fbe63c0f0c19cc95654db2f
Franki <fgomezrosell@gmail.com>
2025-05-12 20:40:17 -0300

    Clases de TypeScript completas, con entorno y planificacion creados

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const num1 = 5;
+const num2 = 15;
+// const sumar = (a: Number, b: Number): Number => a + b;
+// console.log(sumar(num1, num2));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const num1: Number = 5;
+const num2: Number = 15;
+
+// const sumar = (a: Number, b: Number): Number => a + b;
+
+// console.log(sumar(num1, num2));
