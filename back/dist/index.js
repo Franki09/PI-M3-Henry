ee84fd7a969ddb3abb71228ccb3f94214f4946a4
Franki <fgomezrosell@gmail.com>
2025-05-13 11:43:04 -0300

    TypeScript corregido (Ahora esta en el back)

--- back/dist/index.js
+++ back/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 const num1 = 5;
 const num2 = 15;
-// const sumar = (a: Number, b: Number): Number => a + b;
+// const sumar = (a: Number, b: Number): Number => {
+//     return a + b
+// }
 // console.log(sumar(num1, num2));
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,8 @@
 const num1: Number = 5;
 const num2: Number = 15;
 
-// const sumar = (a: Number, b: Number): Number => a + b;
+// const sumar = (a: Number, b: Number): Number => {
+//     return a + b
+// }
 
 // console.log(sumar(num1, num2));
