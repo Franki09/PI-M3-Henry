a6b57957446a32daff47d0ca7cf9889d09306ec9
Franki <fgomezrosell@gmail.com>
2025-05-21 01:28:38 -0300

    Se inicio el proyecto de front. Se creo Vite + React y se crearon algunos modulos. Aunque no puedo conectarlos bien.

---
+++ front/Front M3/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+  return (
+    <>
+      <h3> Este es el NavBar de mi pagina</h3>
+      <span>Home</span>
+      <span>Mis Turnos</span>
+      <span>Cerrar Sesion</span>
+    </>
+  );
+};
